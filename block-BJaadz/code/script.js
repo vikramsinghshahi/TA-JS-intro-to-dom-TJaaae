e1034cb0c833bc6f946183e6a5cf430b37612deb
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-06-15 23:21:52 +0530

    acessing dom element

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,10 +1,22 @@
 // Select the h1 element and store it in a variable named heading.
+let heading = document.querySelector(`h1`);
+
+
 
 // Check the typeof heading and log it.
 
+console.log(typeof heading);
+
 // Change the color of heading to black.
 
+heading.style.color = "black" ;
+
+
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
+
+let allHrs = document.querySelectorAll(`hr`);
+
+console.log(allHrs);
 
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
@@ -15,19 +27,51 @@ HINT:
 let allHrsArray = Array.from(allHrs)
 */
 
+let allHrsArray = Array.from(allHrs);
+
+console.log(allHrsArray);
+
+
+
 // Set the border of the all the hr elements to "1px solid tomato"
+
+allHrsArray.forEach((ele) => ele.style.border = "1px solid tomato" );
+
 
 // Change the background of all the hr to "antiquewhite" using for of loop.
 
+for( i = 0 ; i< allHrsArray.length ; i++){
+  allHrsArray[i].style.backgroundColor = "antiquewhite" ;
+}
+
 // Change the 'border-radius' of all the hr to "5px" using array.
+
+
+allHrsArray.forEach((ele) => ele.style.borderRadius = "5px" );
+
+
 
 // Change the alignment of the heading(h1) to center.
 
+heading.style.textAlign = "center" ;
+
 // Change the font size of the heading to 3rem.
+
+heading.style.fontSize = "3rem";
+
 
 // Change the border of hr with class 'image' to `2px solid purple`.
 
+let allHrWithImage = document.querySelectorAll(`.image`);
+
+console.log(allHrWithImage)
+
+
+allHrWithImage.forEach((ele) => ele.style.border= "2px solid purple");
+
+
 // Hide the box number 17 (last box).
+
 
 // Change the border of all the hr element from solid to dashed type
 

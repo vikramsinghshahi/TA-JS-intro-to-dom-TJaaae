fc3e0e863a1e455b3cb422759eda97cb407776e2
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-06-18 14:43:56 +0530

    update

---
+++ block-BJaaeh/code/script.js
@@ -0,0 +1,19 @@
+
+const secondHand = document.querySelector(`.second-hand`);
+
+
+function setDate() {
+     const now = new Date();
+
+     const seconds = now.getSeconds();
+    
+     const secondsDegrees  = ((seconds/60)* 360) + 90;
+    
+     secondHand.style.transform = `rotate(${secondsDegrees}degre)`;
+
+     console.log(seconds);
+     
+
+}
+
+setInterval( setDate ,1000);
